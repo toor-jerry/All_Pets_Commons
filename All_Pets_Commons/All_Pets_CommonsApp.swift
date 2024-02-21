769957e594b192533569689872223f72b8cd2b29
toor-jerry <gerarchicharo37@gmail.com>
2024-02-20 21:27:41 -0600

    Add colors commons

---
+++ All_Pets_Commons/All_Pets_CommonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_Pets_CommonsApp.swift
+//  All_Pets_Commons
+//
+//  Created by Gerardo Bautista Castaneda on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct All_Pets_CommonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
