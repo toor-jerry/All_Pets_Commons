4db1ef78a1721c12af32ed7896d7d02ce968ad68
toor-jerry <gerarchicharo37@gmail.com>
2024-02-21 11:33:11 -0600

    Add color to extension

--- AllPetsCommons/Sources/AllPetsCommons/Color+Extension.swift
+++ AllPetsCommons/Sources/AllPetsCommons/Color+Extension.swift
@@ -10,4 +10,12 @@ import SwiftUI
 public extension Color {
     
     static let bluePrincipal = Color(.bluePrincipal)
+    static let backgroundPrincipal = Color(.backgroundPrincipal)
+    static let brownPrincipal = Color(.brownPrincipal)
+    static let brownSecondary = Color(.brownSecondary)
+    static let redPrincipal = Color(.redPrincipal)
+    static let grayPrincipal = Color(.grayPrincipal)
+    static let limeGreen = Color(.limeGreen)
+    static let pinkPrincipal = Color(.pinkPrincipal)
+    static let purpleSecundary = Color(.purpleSecundary)
 }
