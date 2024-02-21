95a32fc4c89821e35cd5d15144fc57a5115d6aab
toor-jerry <gerarchicharo37@gmail.com>
2024-02-21 12:48:51 -0600

    Add generic ImagePicker

---
+++ AllPetsCommons/Sources/AllPetsCommons/Generics/ImagePicker.swift
@@ -0,0 +1,52 @@
+//  ImagePicker.swift
+//  All_Pets
+//
+//  Created by Gerardo Bautista Castañeda on 15/08/23.
+//  Copyright © 2023 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import SwiftUI
+
+public struct ImagePicker: UIViewControllerRepresentable {
+    
+    @Environment(\.presentationMode) private var presentationMode
+    
+    var sourceType: UIImagePickerController.SourceType = .photoLibrary
+    @Binding var selectedImage: UIImage
+    
+    public func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        
+        let imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = sourceType
+        imagePicker.delegate = context.coordinator
+        
+        return imagePicker
+    }
+    
+    public func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+        
+    }
+    
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    public final class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        
+        var parent: ImagePicker
+        
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+        
+        public func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            
+            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
