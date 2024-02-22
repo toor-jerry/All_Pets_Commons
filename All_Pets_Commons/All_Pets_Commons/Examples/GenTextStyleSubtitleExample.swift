cfb9c0795ec2e7dab7188b054778fa9112498815
toor-jerry <gerarchicharo37@gmail.com>
2024-02-21 18:06:39 -0600

    Add GenTextStyleSubtitle

---
+++ All_Pets_Commons/All_Pets_Commons/Examples/GenTextStyleSubtitleExample.swift
@@ -0,0 +1,16 @@
+//  GenTextStyleSubtitleExample.swift
+//  All_Pets_Commons
+//
+//  Created by Gerardo Bautista Castañeda on 21/02/24.
+//  Copyright © 2024 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+class GenTextStyleSubtitleExample {
+    
+    func templateFunction() {
+        
+    }
+    
+}
